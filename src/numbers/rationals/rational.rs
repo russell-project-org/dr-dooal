468eb9b0a2aa178b76ea34d7e6af40599051ea17
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-06-29 18:02:26 +0800

    Add operations for Rationals

--- src/numbers/rationals/rational.rs
+++ src/numbers/rationals/rational.rs
@@ -1,7 +1,9 @@
 use std::fmt;
+use std::ops:: {Add, Sub, Mul, Div};
+use std::cmp:: {PartialEq, Eq};
 use super::dooal_utils::Math;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Rational {
     pub numerator: i32,
     pub denominator: i32
@@ -42,5 +44,101 @@ impl fmt::Display for Rational {
             (x, 1) => write!(f, "{}", x),
             (x, y) => write!(f, "{}/{}", x, y)
         }  
+    }
+}
+
+impl PartialEq for Rational {
+    fn eq(&self, other: &Rational) -> bool {
+        self.numerator * other.denominator == self.denominator * other.numerator
+    }
+}
+ 
+impl Eq for Rational {}
+
+impl Add for Rational {
+    type Output = Rational;
+    fn add(self, other: Rational) -> Rational {
+        Rational::new(self.numerator * other.denominator + self.denominator * other.numerator,
+            self.denominator * other.denominator).unwrap()
+    }
+}
+
+impl Add<i32> for Rational {
+    type Output = Rational;
+    fn add(self, other: i32) -> Rational {
+        Rational::add(self, Rational{numerator: other, denominator: 1})
+    }
+}
+
+impl Add<Rational> for i32 {
+    type Output = Rational;
+    fn add(self, rhs: Rational) -> Rational {
+        Rational::add(Rational{numerator: self, denominator: 1}, rhs)
+    }
+}
+
+impl Sub for Rational {
+    type Output = Rational;
+    fn sub(self, other: Rational) -> Rational {
+        Rational::new(self.numerator * other.denominator - self.denominator * other.numerator,
+            self.denominator * other.denominator).unwrap()
+    }
+}
+
+impl Sub<Rational> for i32 {
+    type Output = Rational;
+    fn sub(self, rhs: Rational) -> Rational {
+        Rational::sub(Rational { numerator: self, denominator: 1 }, rhs)
+    }
+}
+
+impl Sub<i32> for Rational {
+    type Output = Rational;
+    fn sub(self, other: i32) -> Rational {
+        Rational::sub(self, Rational {numerator: other, denominator: 1})
+    }
+}
+
+impl Mul for Rational {
+    type Output = Rational;
+    fn mul(self, other: Rational) -> Rational {
+        Rational::new(self.numerator * other.numerator,
+            self.denominator * other.denominator).unwrap()
+    }
+}
+
+impl Mul<i32> for Rational {
+    type Output = Rational;
+    fn mul(self, other: i32) -> Rational {
+        Rational::mul(self, Rational{numerator: other, denominator: 1})
+    }
+}
+
+impl Mul<Rational> for i32 {
+    type Output = Rational;
+    fn mul(self, other: Rational) -> Rational {
+        Rational::mul(Rational { numerator: self, denominator: 1 }, other)
+    }
+}
+
+impl Div for Rational {
+    type Output = Rational;
+    fn div(self, other: Rational) -> Rational {
+        Rational::new(self.numerator * other.denominator,
+            self.denominator * other.numerator).unwrap()
+    }
+}
+
+impl Div<i32> for Rational {
+    type Output = Rational;
+    fn div(self, other: i32) -> Rational {
+        Rational::div(self, Rational {numerator: other, denominator: 1})
+    }
+}
+
+impl Div<Rational> for i32 {
+    type Output = Rational;
+    fn div(self, other: Rational) -> Rational {
+        Rational::div(Rational { numerator: self, denominator: 1 }, other)
     }
 }
