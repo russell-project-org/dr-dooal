c96cc2ad954b8a7e800ee6ccc17ff8fd253a501d
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-06-29 18:01:54 +0800

    Delete dead code

--- src/main.rs
+++ src/main.rs
@@ -37,42 +37,12 @@ fn main() {
 }
 
 fn test() {
-    let r: Rational = Rational::new(0, 2).unwrap_or_else(|_err| {
+    let r: Rational = Rational::new(3, 2).unwrap_or_else(|_err| {
         print!("Rational got problem");
         process::exit(1);
     });
-    println!("{}",r);
+
+    let s = Rational {numerator: 2, denominator: 8};
+    println!("{}",r + s);
 }
 
-// Dead code here for experiments
-fn number_guesser() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
-}
