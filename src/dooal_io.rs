051f74978c583b6d7ecf228d1fca7bcaf3553ee4
Tan Kai Min Russell <russelltanreg38@gmail.com>
2023-06-30 05:48:45 +0800

    Add Flags file and change Config

--- src/dooal_io.rs
+++ src/dooal_io.rs
@@ -1,3 +1,4 @@
 pub mod reader;
 pub mod config;
 pub mod parser;
+pub mod flags;
---
+++ src/dooal_io/flags.rs
@@ -0,0 +1,17 @@
+pub enum Flag {
+    Help,
+    Solve,
+    Convert
+}
+
+pub struct Help {
+
+}
+
+pub struct Solve {
+
+}
+
+pub struct Convert {
+
+}
